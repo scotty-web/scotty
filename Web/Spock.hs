bcca269650276c16ccfea4cbcfd5e285d33bd72c
Andrew Farmer <anfarmer@ku.edu>
2012-01-02 21:02:11 -0600

    Day 2: Abstract our monads, add ability to throw errors and redirect, commit to Text over ByteString, more examples, bigger API.

--- Web/Spock.hs
+++ Web/Spock.hs
@@ -1,21 +1,24 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
 module Web.Spock
-    ( spock
-    , middleware
-    , get, post, put, delete, addroute
-    , text, html, file, json
-    , request, params, param
-    , runAction -- remove
+    ( spock, spockApp -- spock-to-WAI
+    , SpockM, ActionM -- Types
+    , middleware, get, post, put, delete, addroute -- building apps
+    , request, params, param -- getting input
+    , status, header, redirect -- editing response and redirect
+    , text, html, file, json -- setting output
+    , raise, rescue          -- error handling
     ) where
 
 import Blaze.ByteString.Builder (fromByteString, fromLazyByteString)
 
 import Control.Applicative
+import Control.Monad.Error
 import Control.Monad.Reader
 import qualified Control.Monad.State as MS
 
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.CaseInsensitive as CI
 import Data.Default
 import Data.Maybe
 import Data.Monoid
@@ -36,88 +39,128 @@ data SpockState = SpockState {
 instance Default SpockState where
     def = SpockState [] []
 
-type SpockT a = MS.StateT SpockState IO a
-type SpockM = SpockT ()
+newtype SpockM a = S { runS :: MS.StateT SpockState IO a }
+    deriving (Monad, Functor, MS.MonadState SpockState)
 
-spock :: Port -> SpockM -> IO ()
-spock p app = do
-    putStrLn "Setting phasers to stun..."
-    ks <- MS.execStateT app def
-    run p $ foldr ($) notFoundApp $ middlewares ks ++ routes ks
+spock :: Port -> SpockM () -> IO ()
+spock p s = putStrLn "Setting phasers to stun..." >> (run p =<< spockApp s)
+
+spockApp :: SpockM () -> IO Application
+spockApp defs = do
+    s <- MS.execStateT (runS defs) def
+    return $ foldl (flip ($)) notFoundApp $ middlewares s ++ routes s
 
 notFoundApp :: Application
 notFoundApp _ = return $ setStatus status404 $ htmlResponse "404: File Not Found!"
 
-middleware :: Middleware -> SpockM
+middleware :: Middleware -> SpockM ()
 middleware m = MS.modify (\ (SpockState ms rs) -> SpockState (m:ms) rs)
 
 type Param = (T.Text, T.Text)
 
-type ActionT = ReaderT (Request,[Param]) (MS.StateT Response IO)
-type ActionM = ActionT ()
+data ActionError = Redirect T.Text
+                 | ActionError T.Text
+    deriving (Eq,Show)
 
-request :: ActionT Request
+instance Error ActionError where
+    strMsg = ActionError . T.pack
+
+newtype ActionM a = AM { runAM :: ErrorT ActionError (ReaderT (Request,[Param]) (MS.StateT Response IO)) a }
+    deriving ( Monad, MonadIO, MonadFix, Functor
+             , MonadReader (Request,[Param]), MS.MonadState Response, MonadError ActionError)
+
+runAction :: [Param] -> ActionM () -> Application
+runAction ps action req = lift $ flip MS.execStateT def $ runReaderT (runErrorT $ runAM $ handleErrors action) (req,ps)
+
+handleErrors :: ActionM () -> ActionM ()
+handleErrors action = catchError action $ \e ->
+    case e of
+        Redirect url -> do
+            status status302
+            header "Location" url
+        ActionError msg -> do
+            html $ mconcat ["<h1>500 Internal Server Error</h1>", msg]
+            status status500
+
+raise :: T.Text -> ActionM a
+raise = throwError . ActionError
+
+rescue :: ActionM a -> (T.Text -> ActionM a) -> ActionM a
+rescue action handler = catchError action $ \e -> case e of
+    ActionError msg -> handler msg  -- handle errors
+    r               -> throwError r -- rethrow redirects
+
+request :: ActionM Request
 request = fst <$> ask
 
-params :: ActionT [Param]
+params :: ActionM [Param]
 params = snd <$> ask
 
-param :: T.Text -> ActionT (Maybe T.Text)
-param k = lookup k <$> params
+param :: T.Text -> ActionM T.Text
+param k = do
+    val <- lookup k <$> params
+    maybe (raise $ mconcat ["Param: ", k, " not found!"]) return val
 
-runAction :: [Param] -> ActionM -> Application
-runAction ps action req = lift $ flip MS.execStateT def $ runReaderT action (req,ps)
-
-get, post, put, delete :: BS.ByteString -> ActionM -> SpockM
+get, post, put, delete :: T.Text -> ActionM () -> SpockM ()
 get    = addroute GET
 post   = addroute POST
 put    = addroute PUT
 delete = addroute DELETE
 
-addroute :: StdMethod -> BS.ByteString -> ActionM -> SpockM
+addroute :: StdMethod -> T.Text -> ActionM () -> SpockM ()
 addroute method path action = MS.modify (\ (SpockState ms rs) -> SpockState ms (r:rs))
     where r = route method path action
 
--- todo: captures and wildcards
-route :: StdMethod -> BS.ByteString -> ActionM -> Middleware
+-- todo: wildcards?
+route :: StdMethod -> T.Text -> ActionM () -> Middleware
 route method path action app req =
     if Right method == parseMethod (requestMethod req)
-    then case matchRoute path (rawPathInfo req) of
+    then case matchRoute path (E.decodeUtf8 $ rawPathInfo req) of
             Just params -> runAction (addQueryParams req params) action req
             Nothing -> app req
     else app req
 
 addQueryParams req = (++ [ (k, fromMaybe "" v) | (k,v) <- parseQueryText (rawQueryString req) ])
 
-matchRoute :: BS.ByteString -> BS.ByteString -> Maybe [Param]
-matchRoute pat req = go (BS.split '/' pat) (BS.split '/' req) []
+matchRoute :: T.Text -> T.Text -> Maybe [Param]
+matchRoute pat req = go (T.split (=='/') pat) (T.split (=='/') req) []
     where go [] [] ps = Just ps -- request string and pattern match!
-          go [] r  ps | BS.null (mconcat r)  = Just ps -- in case request has trailing slashes
-                      | otherwise            = Nothing -- request string is longer than pattern
-          go p  [] ps | BS.null (mconcat p)  = Just ps -- in case pattern has trailing slashes
-                      | otherwise            = Nothing -- request string is not long enough
-          go (p:ps) (r:rs) prs | p == r           = go ps rs prs -- equal literals, keeping checking
-                               | BS.null p        = Nothing      -- p is null, but r is not, fail
-                               | BS.head p == ':' = go ps rs $ (E.decodeUtf8 $ BS.tail p, E.decodeUtf8 r) : prs -- p is a capture, add to params
-                               | otherwise        = Nothing      -- both literals, but unequal, fail
+          go [] r  ps | T.null (mconcat r)  = Just ps -- in case request has trailing slashes
+                      | otherwise           = Nothing -- request string is longer than pattern
+          go p  [] ps | T.null (mconcat p)  = Just ps -- in case pattern has trailing slashes
+                      | otherwise           = Nothing -- request string is not long enough
+          go (p:ps) (r:rs) prs | p == r          = go ps rs prs -- equal literals, keeping checking
+                               | T.null p        = Nothing      -- p is null, but r is not, fail
+                               | T.head p == ':' = go ps rs $ (T.tail p, r) : prs
+                                                                -- p is a capture, add to params
+                               | otherwise       = Nothing      -- both literals, but unequal, fail
 
-text :: T.Text -> ActionM
+status :: Status -> ActionM ()
+status = MS.modify . setStatus
+
+header :: T.Text -> T.Text -> ActionM ()
+header k v = MS.modify $ setHeader (CI.mk $ E.encodeUtf8 k, E.encodeUtf8 v)
+
+redirect :: T.Text -> ActionM ()
+redirect = throwError . Redirect
+
+text :: T.Text -> ActionM ()
 text = MS.put . textResponse
 
-html :: T.Text -> ActionM
+html :: T.Text -> ActionM ()
 html t = text t >> MS.modify (setHeader ("Content-Type", "text/html"))
 
-file :: FilePath -> ActionM
+file :: FilePath -> ActionM ()
 file = MS.put . fileResponse
 
-json :: (A.ToJSON a) => a -> ActionM
+json :: (A.ToJSON a) => a -> ActionM ()
 json val = MS.put $ ResponseBuilder status200
                                     [ ("Content-Type", "application/json") ]
                                     $ fromLazyByteString $ A.encode val
 
--- TODO: look up MIME type based on extension
+-- TODO: look up MIME type based on extension?
 fileResponse :: FilePath -> Response
-fileResponse fp = ResponseFile status200 [ ("Content-Type", "text/html") ] fp Nothing
+fileResponse fp = ResponseFile status200 [] fp Nothing
 
 textResponse :: T.Text -> Response
 textResponse t = ResponseBuilder status200
--- testapp.hs
+++ testapp.hs
@@ -7,22 +7,65 @@ import Control.Monad.Trans
 import Data.Monoid
 import System.Random
 
+import Network.HTTP.Types (status302)
+
 main = spock 3000 $ do
+    -- Add any WAI middleware, they are run top-down.
     middleware logStdoutDev
 
+    -- To demonstrate that routes are matched top-down.
     get "/" $ text "foobar"
+    get "/" $ text "barfoo"
 
+    -- Using a parameter in the query string. If it has
+    -- not been given, a 500 page is generated.
     get "/foo" $ do
-        Just v <- param "fooparam"
+        v <- param "fooparam"
         html $ mconcat ["<h1>", v, "</h1>"]
 
+    -- An uncaught error becomes a 500 page.
+    get "/raise" $ raise "some error here"
+
+    -- You can set status and headers directly.
+    get "/redirect-custom" $ do
+        status status302
+        header "Location" "http://www.google.com"
+        -- note first arg to header is NOT case-sensitive
+
+    -- redirects preempt execution
+    get "/redirect" $ do
+        redirect "http://www.google.com"
+        raise "this error is never reached"
+
+    -- Of course you can catch your own errors.
+    get "/rescue" $ do
+        (do raise "a rescued error"; redirect "http://www.we-never-go-here.com")
+        `rescue` (\m -> text $ "we recovered from " `mappend` m)
+
+    -- Parts of the URL that start with a colon match
+    -- any string, and capture that value as a parameter.
+    -- URL captures take precedence over query string parameters.
     get "/foo/:bar/required" $ do
-        Just v <- param "bar"
+        v <- param "bar"
         html $ mconcat ["<h1>", v, "</h1>"]
 
+    -- Files are streamed directly to the client.
     get "/404" $ file "404.html"
 
+    -- You can do IO with liftIO, and you can return JSON content.
     get "/random" $ do
         g <- liftIO newStdGen
         json $ take 20 $ randomRs (1::Int,100) g
+
+{- If you don't want to use Warp as your webserver,
+   you can use any WAI handler.
+
+import Network.Wai.Handler.FastCGI (run)
+
+main = do
+    myApp <- spockApp $ do
+        get "/" $ text "hello world"
+
+    run myApp
+-}
 
