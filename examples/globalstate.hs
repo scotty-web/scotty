345f911adbf3039317f773f5c03ef3c246d5db2a
Andrew Farmer <anfarmer@ku.edu>
2013-09-01 23:56:38 -0500

    Use the ReaderT/TVar solution for globalstate example rather than StateT/MVar.

--- examples/globalstate.hs
+++ examples/globalstate.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
--- | An example of embedding a custom state monad into
--- Scotty's transformer stack, using an MVar to synchronize
--- the state globally.
+-- An example of embedding a custom monad into
+-- Scotty's transformer stack, using ReaderT to provide access
+-- to a TVar containing global state.
 --
 -- Note: this example is somewhat simple, as our top level
 -- is IO itself. The types of 'scottyT' and 'scottyAppT' are
@@ -9,8 +9,8 @@
 -- embedded into any MonadIO monad.
 module Main where
 
-import Control.Concurrent.MVar
-import Control.Monad.State hiding (get)
+import Control.Concurrent.STM
+import Control.Monad.Reader 
 
 import Data.Default
 import Data.Text.Lazy (pack)
@@ -24,23 +24,39 @@ newtype AppState = AppState { tickCount :: Int }
 instance Default AppState where
     def = AppState 0
 
-newtype WebM a = WebM { runWebM :: StateT AppState IO a }
-    deriving (Monad, MonadIO, MonadState AppState)
+-- Why 'ReaderT (TVar AppState)' rather than 'StateT AppState'?
+-- With a state transformer, 'runActionToIO' (below) would have
+-- to provide the state to _every action_, and save the resulting
+-- state, using an MVar. This means actions would be blocking,
+-- effectively meaning only one request could be serviced at a time.
+-- The 'ReaderT' solution means only actions that actually modify 
+-- the state need to block/retry.
+-- 
+-- Also note: your monad must be an instance of 'MonadIO' for
+-- Scotty to use it.
+newtype WebM a = WebM { runWebM :: ReaderT (TVar AppState) IO a }
+    deriving (Monad, MonadIO, MonadReader (TVar AppState))
 
+-- Scotty's monads are layered on top of our custom monad.
+-- We define this synonym for lift in order to be explicit
+-- about when we are operating at the 'WebM' layer.
 webM :: MonadTrans t => WebM a -> t WebM a
 webM = lift
 
+-- Some helpers to make this feel more like a state monad.
+gets :: (AppState -> b) -> WebM b
+gets f = ask >>= liftIO . readTVarIO >>= return . f
+
+modify :: (AppState -> AppState) -> WebM ()
+modify f = ask >>= liftIO . atomically . flip modifyTVar' f
+
+main :: IO ()
 main = do
-    db <- newEmptyMVar
+    sync <- newTVarIO def
         -- Note that 'runM' is only called once, at startup.
-    let runM m = do (r,s) <- runStateT (runWebM m) def
-                    putMVar db s
-                    return r
+    let runM m = runReaderT (runWebM m) sync
         -- 'runActionToIO' is called once per action.
-        runActionToIO m = do s <- takeMVar db
-                             (r,s') <- runStateT (runWebM m) s
-                             putMVar db s'
-                             return r
+        runActionToIO = runM
 
     scottyT 3000 runM runActionToIO $ do
         middleware logStdoutDev
