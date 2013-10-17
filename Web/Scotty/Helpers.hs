6f30314f56f1832f91c0a9b76e2f519ada1f2b3d
Daniil Frumin <difrumin@gmail.com>
2013-10-17 21:41:48 +0400

    Adding 'paramSafe' and 'paramEscaped' to the Helpers module

--- Web/Scotty/Helpers.hs
+++ Web/Scotty/Helpers.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | Miscellaneous convenience functions. If you create a helper while using
 -- Scotty that you find useful, feel free to submit a patch to this file.
 module Web.Scotty.Helpers
     ( addQueryString
+    , paramSafe
+    , paramEscaped
     ) where
 
 import           Control.Monad
@@ -23,3 +26,26 @@ import           Web.Scotty.Util
 --
 addQueryString :: Monad m => T.Text -> ActionT m T.Text
 addQueryString r = liftM ((r <>) . strictByteStringToLazyText . Wai.rawQueryString) request
+
+-- * Additional functions for getting the parameters
+
+-- | Like 'param', but returns @Maybe@ instead of doing 'raise'.
+paramSafe :: (Monad m, Functor m, Parsable a) => T.Text -> ActionT m (Maybe a)
+paramSafe q = fmap Just (param q)
+              `rescue` \_ -> return Nothing
+
+-- | Like 'param', but escapes the special characters and replaces
+-- them with HTML entities
+paramEscaped :: (Monad m, Functor m) => T.Text -> ActionT m T.Text
+paramEscaped = (fmap . fmap) (T.concatMap escape) param
+
+escape :: Char -> T.Text
+escape h
+    | h == '&' = "&amp;"
+    | h == '\\' = "&#92;"
+    | h == '"' = "&quot;"
+    | h == '\'' = "&#39;"
+    | h == '<' = "&lt;"
+    | h == '>' = "&gt;"
+    | otherwise = T.singleton h
+
