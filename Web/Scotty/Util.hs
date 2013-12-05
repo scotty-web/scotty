e5ca96dec7275fcd87741266d8b5a31eee220fe1
Andrew Farmer <anfarmer@ku.edu>
2013-12-05 02:03:05 -0600

    Overhaul for Wai 2.0, take away access to monad internals, beginning of custom error types.

--- Web/Scotty/Util.hs
+++ Web/Scotty/Util.hs
@@ -4,7 +4,7 @@ module Web.Scotty.Util
     , setContent
     , setHeaderWith
     , setStatus
-    , Content(..)
+    , mkResponse
     , replace
     , add
     ) where
@@ -13,17 +13,11 @@ import Network.Wai
 
 import Network.HTTP.Types
 
-import Blaze.ByteString.Builder (Builder)
-import Data.Conduit (Flush, Source, ResourceT)
-import Data.Default
-import Data.Monoid
-
 import qualified Data.ByteString as B
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Encoding as ES
 
-instance Default Response where
-    def = ResponseBuilder status200 [] mempty
+import Web.Scotty.Types
 
 lazyTextToStrictByteString :: T.Text -> B.ByteString
 lazyTextToStrictByteString = ES.encodeUtf8 . T.toStrict
@@ -31,30 +25,22 @@ lazyTextToStrictByteString = ES.encodeUtf8 . T.toStrict
 strictByteStringToLazyText :: B.ByteString -> T.Text
 strictByteStringToLazyText = T.fromStrict . ES.decodeUtf8
 
-data Content = ContentBuilder Builder
-             | ContentFile FilePath
-             | ContentSource (Source (ResourceT IO) (Flush Builder))
+setContent :: Content -> ScottyResponse -> ScottyResponse
+setContent c sr = sr { srContent = c }
 
-setContent :: Content -> Response -> Response
-setContent (ContentBuilder b)  (ResponseBuilder s h _) = ResponseBuilder s h b
-setContent (ContentBuilder b)  (ResponseFile s h _ _)  = ResponseBuilder s h b
-setContent (ContentBuilder b)  (ResponseSource s h _)  = ResponseBuilder s h b
-setContent (ContentFile f)     (ResponseBuilder s h _) = ResponseFile s h f Nothing
-setContent (ContentFile f)     (ResponseFile s h _ _)  = ResponseFile s h f Nothing
-setContent (ContentFile f)     (ResponseSource s h _)  = ResponseFile s h f Nothing
-setContent (ContentSource src) (ResponseBuilder s h _) = ResponseSource s h src
-setContent (ContentSource src) (ResponseFile s h _ _)  = ResponseSource s h src
-setContent (ContentSource src) (ResponseSource s h _)  = ResponseSource s h src
+setHeaderWith :: ([(HeaderName, B.ByteString)] -> [(HeaderName, B.ByteString)]) -> ScottyResponse -> ScottyResponse
+setHeaderWith f sr = sr { srHeaders = f (srHeaders sr) }
 
-setHeaderWith :: ([(HeaderName, B.ByteString)] -> [(HeaderName, B.ByteString)]) -> Response -> Response
-setHeaderWith g (ResponseBuilder s h b) = ResponseBuilder s (g h) b
-setHeaderWith g (ResponseFile s h f fp) = ResponseFile s (g h) f fp
-setHeaderWith g (ResponseSource s h cs) = ResponseSource s (g h) cs
+setStatus :: Status -> ScottyResponse -> ScottyResponse
+setStatus s sr = sr { srStatus = s }
 
-setStatus :: Status -> Response -> Response
-setStatus s (ResponseBuilder _ h b) = ResponseBuilder s h b
-setStatus s (ResponseFile _ h f fp) = ResponseFile s h f fp
-setStatus s (ResponseSource _ h cs) = ResponseSource s h cs
+mkResponse :: ScottyResponse -> Response
+mkResponse sr = case srContent sr of
+                    ContentBuilder b  -> responseBuilder s h b
+                    ContentFile f     -> responseFile s h f Nothing
+                    ContentSource src -> responseSource s h src
+    where s = srStatus sr
+          h = srHeaders sr
 
 -- Note: we assume headers are not sensitive to order here (RFC 2616 specifies they are not)
 replace :: (Eq a) => a -> b -> [(a,b)] -> [(a,b)]
