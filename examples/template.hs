a238a202d599d381d7f7f5da876eedc7c2034a57
Simon <Simon Lindhn>
2015-03-23 12:59:36 +0100

    Added examples for using templates

---
+++ examples/template.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
+
+main = scotty 3000 $
+  get "/" serve
+
+serve = do
+  template "template.html"
+  tSet "loop" $ List [Value "hej1",Value "hej2"]
+  tSet "map" $ fromPairs [("k",Value "v")]
+  tSet "bool" (TIf True)
+  tSet "test" $ Value "hej"
+
+
+
+
