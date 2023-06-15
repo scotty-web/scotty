ca588b2b7dc7de7dd591d8184f8a7ccb1b53fa40
Marco Zocca <marco.zocca@unfoldml.com>
2023-06-15 08:57:27 +0200

    adapt scotty-cookies example program to use new module as well

--- examples/cookies.hs
+++ examples/cookies.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
--- This examples requires you to: cabal install cookie
--- and: cabal install blaze-html
+-- This examples requires you to: cabal install blaze-html
 module Main (main) where
 
 import Control.Monad (forM_)
@@ -14,23 +13,7 @@ import qualified Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Web.Scotty
-import Web.Cookie
-
-makeCookie :: BS.ByteString -> BS.ByteString -> SetCookie
-makeCookie n v = def { setCookieName = n, setCookieValue = v }
-
-renderSetCookie' :: SetCookie -> Text
-renderSetCookie' = T.decodeUtf8 . B.toLazyByteString . renderSetCookie
-
-setCookie :: BS.ByteString -> BS.ByteString -> ActionM ()
-setCookie n v = setHeader "Set-Cookie" (renderSetCookie' (makeCookie n v))
-
-getCookies :: ActionM (Maybe CookiesText)
-getCookies =
-    fmap (fmap (parseCookiesText . lazyToStrict . T.encodeUtf8)) $
-        header "Cookie"
-    where
-        lazyToStrict = BS.concat . BSL.toChunks
+import Web.Scotty.Cookie (CookiesText, setSimpleCookie, getCookies)
 
 renderCookiesTable :: CookiesText -> H.Html
 renderCookiesTable cs =
@@ -48,9 +31,7 @@ main = scotty 3000 $ do
     get "/" $ do
         cookies <- getCookies
         html $ renderHtml $ do
-            case cookies of
-                Just cs -> renderCookiesTable cs
-                Nothing -> return ()
+            renderCookiesTable cookies
             H.form H.! method "post" H.! action "/set-a-cookie" $ do
                 H.input H.! type_ "text" H.! name "name"
                 H.input H.! type_ "text" H.! name "value"
@@ -59,5 +40,5 @@ main = scotty 3000 $ do
     post "/set-a-cookie" $ do
         name'  <- param "name"
         value' <- param "value"
-        setCookie name' value'
+        setSimpleCookie name' value'
         redirect "/"
