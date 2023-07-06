f02c75e88485f81952906c82269a9e58000da9b8
Marco Zocca <marco.zocca@unfoldml.com>
2023-07-06 05:43:24 +0200

    get rid of ActionT.return

--- Web/Scotty/Internal/Types.hs
+++ Web/Scotty/Internal/Types.hs
@@ -164,7 +164,6 @@ newtype ActionT e m a = ActionT { runAM :: ExceptT (ActionError e) (ReaderT Acti
     deriving ( Functor, Applicative, MonadIO )
 
 instance (Monad m, ScottyError e) => Monad.Monad (ActionT e m) where
-    return = ActionT . return
     ActionT m >>= k = ActionT (m >>= runAM . k)
 #if !(MIN_VERSION_base(4,13,0))
     fail = Fail.fail
